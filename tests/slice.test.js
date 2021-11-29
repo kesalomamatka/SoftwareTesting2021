b11a0d5a4dd68dcdee0b3a452e936e82ca7ad4eb
Sanna Salonen <sanna.m.salonen@tuni.fi>
2021-11-29 22:04:19 +0200

    eq and slice adjustments

--- tests/slice.test.js
+++ tests/slice.test.js
@@ -9,46 +9,73 @@ describe('slice.js test', () => {
 
     it('what if no end index is given', () => {
         let array = [1, 2, 3, 4]
-        expect(slice(array,2)).toIncludeSameMembers( [3, 4]);
+        expect(slice(array, 2)).toIncludeSameMembers([3, 4]);
     });
 
     it('what if no end index is given', () => {
         let array = [1, 2, 3, 4]
-        expect(slice(array,0,2)).toIncludeSameMembers( [1, 2]);
+        expect(slice(array, 0, 2)).toIncludeSameMembers([1, 2]);
+    });
+
+    it('what if no start index is given', () => {
+        let array = [1, 2, 3, 4]
+        expect(slice(array, undefined, -1)).toIncludeSameMembers([1, 2, 3, 4]);
     });
 
     it('what if start and end index are the same', () => {
         let array = [1, 2, 3, 4]
-        expect(slice(array,2,2)).toIncludeSameMembers( []);
+        expect(slice(array, 2, 2)).toIncludeSameMembers([]);
     });
 
     it('what if start index is negative and end index is positive', () => {
         let array = [1, 2, 3, 4]
-        expect(slice(array,-2,2)).toThrow(Error);
+        expect(slice(array, -3, 3)).toIncludeSameMembers([2, 3]);
+        expect(slice(array, -1, 1)).toIncludeSameMembers([]);
     });
 
     it('what if start index is positive and end index is negative', () => {
         let array = [1, 2, 3, 4]
-        expect(slice(array,2,-2)).toThrow(Error);
+        expect(slice(array, 1, -1)).toIncludeSameMembers([2, 3]);
+        expect(slice(array, 3, -3)).toIncludeSameMembers([]);
     });
 
     it('what if end index is out of range', () => {
         let array = [1, 2, 3, 4]
-        expect(slice(array,2,10)).toThrow(Error);
+        expect(slice(array, 2, 10)).toIncludeSameMembers([3, 4]);
+    });
+
+    it('what if both start and end index are out of range', () => {
+        let array = [1, 2, 3, 4]
+        expect(slice(array, 9, 10)).toIncludeSameMembers([]);
     });
 
     it('what if start index is a larger than end (positive integers)', () => {
         let array = [1, 2, 3, 4]
-        expect(slice(array,3,2)).toThrow(Error);
+        expect(slice(array, 3, 2)).toIncludeSameMembers([]);
     });
 
     it('what if start index is a larger than end (negative integers)', () => {
         let array = [1, 2, 3, 4]
-        expect(slice(array,-1,-2)).toThrow(Error);
+        expect(slice(array, -1, -2)).toIncludeSameMembers([]);
     });
 
     it('what if index is set to Infinity', () => {
         let array = [1, 2, 3, 4]
-        expect(slice(array,Infinity)).toThrow(Error);
+        expect(slice(array, Infinity)).toIncludeSameMembers([]);
+    });
+
+    it('what if array is a String', () => {
+        let array = "1234"
+        expect(slice(array, 2)).toBe("34");
+    });
+
+    it('what if array is not an Array or a String', () => {
+        let array = 1234
+        expect(slice(array, 2)).toIncludeSameMembers([]);
+    });
+
+    it('what if indices are not Numbers', () => {
+        let array = ['100', '200', '300', '400']
+        expect(slice(array, 2)).toIncludeSameMembers(['300', '400']);
     });
 });
