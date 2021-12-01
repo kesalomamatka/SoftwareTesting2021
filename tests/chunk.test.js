e779f0cdfeb7b331f6cb024c159a02c26cab7c98
Sanna Salonen <sanna.m.salonen@tuni.fi>
2021-12-01 13:49:05 +0200

    new test cases to chunk and clamp

--- tests/chunk.test.js
+++ tests/chunk.test.js
@@ -3,25 +3,19 @@ import * as matchers from 'jest-extended';
 expect.extend(matchers);
 
 describe('chunk.js test', ()=>{
-    it('Input valid array ', ()=>{
+    it('Array can be split evenly', ()=>{
         expect(chunk(['a', 'b', 'c', 'd'], 2)).toIncludeSameMembers([['a','b'],['c','d']])
     });
 
-    it('Input valid array with different size', ()=>{
+    it('Array can\'t be split evenly', ()=>{
         expect(chunk(['a', 'b', 'c', 'd'], 3)).toIncludeSameMembers( [['a', 'b', 'c'], ['d']])
     });
-
-
-    it('Input invalid array ', ()=>{
-        expect(chunk(null, 3)).toThrow(Error)
-    });
-
-    
-    it('Size type is not number (toInteger.js, toFinite.js) ', ()=>{
-        expect(chunk(['a', 'b', 'c', 'd'], 'string')).toBe([])
-    });
         
-    it('Size is greater than array length ', ()=>{
-        expect(chunk(['a', 'b', 'c', 'd'], 5)).toThrow(Error)
+    it('Size is greater than array length', ()=>{
+        expect(chunk(['a', 'b', 'c', 'd'], 5)).toIncludeSameMembers(['a', 'b', 'c', 'd'])
+    });
+
+    it('Array is empty', ()=>{
+        expect(chunk([], 3)).toIncludeSameMembers([])
     });
 })
